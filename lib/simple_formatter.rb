82f14afd1e8a8ce68570ec8e643e74a1cf9333f4
Benjamin Wofford <woffordben1992@gmail.com>
2019-11-15 17:23:02 -0500

    Added the get last name method

--- lib/simple_formatter.rb
+++ lib/simple_formatter.rb
@@ -3,11 +3,21 @@ require "byebug"
 
 def ask_first_name
 
-    print "What's your first name? "
-    first_name = gets.chomp
-    first_name.capitalize!
-    return first_name
+        print "What's your first name? "
+        first_name = gets.chomp
+        first_name.capitalize!
+        return first_name
+    
+end
+
+def ask_last_name
+
+    print "What's your last name?"
+    last_name = gets.chomp
+    last_name.capitalize!
+    return last_name
 
 end
 
-puts ask_first_name
+puts "Your name is #{ask_first_name} #{ask_last_name} "
+
