4e9f6b43bd7bb30e142458cab9ea5a53a79239bb
Benjamin Wofford <woffordben1992@gmail.com>
2019-11-15 17:40:47 -0500

    deleted rspec and decided not to test project

--- lib/simple_formatter.rb
+++ lib/simple_formatter.rb
@@ -12,12 +12,21 @@ end
 
 def ask_last_name
 
-    print "What's your last name?"
+    print "What's your last name? "
     last_name = gets.chomp
     last_name.capitalize!
     return last_name
 
 end
 
-puts "Your name is #{ask_first_name} #{ask_last_name} "
+def ask_state_born 
+
+    print "What state were you born in? (Ex: GA, NY, NC..etc) "
+    state = gets.chomp
+    state.upcase!
+    return state
+
+end
+
+puts "Your name is #{ask_first_name} #{ask_last_name} and you were born in #{ask_state_born} "
 
